dad9c866dbfbaf9a5c6065c9a3c89585255a9517
RIZKI <rizkiriswandi33@gmail.com>
2025-05-31 16:17:21 +0700

    Perubahan pada index.jsx

--- src/pages/siswa/mata-pelajaran/index.jsx
+++ src/pages/siswa/mata-pelajaran/index.jsx
@@ -1,15 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import {} from "@mui/material";
-// import {} from "@mui/icons-material";
-// import {} from "react-router-dom";
-
-// export default function MataPelajaran() {
-//   return (
-//     <div>
-//       <h1>Mata Pelajaran</h1>
-//     </div>
-//   );
-// }
 import React, { useState } from "react";
 import { Typography, Box } from "@mui/material";
 
